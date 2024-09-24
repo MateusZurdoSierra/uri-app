8b20a8f772ab18a79e8cc7c0867335f38b38127b
MateusZurdoSierra <mateus.zurdo@superlogica.com>
2024-09-24 00:14:14 -0300

    add location in maps and realize pre build

--- src/presentation/screens/onboarding/ConfirmLocation/index.tsx
+++ src/presentation/screens/onboarding/ConfirmLocation/index.tsx
@@ -1,4 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import {
+	useForegroundPermissions,
+	watchPositionAsync,
+	LocationAccuracy,
+	LocationSubscription,
+	LocationObjectCoords,
+} from 'expo-location';
+
+import Footer from '../../../../modules/global/components/Footer';
+import { getAddressLocation } from '../../../../modules/global/utils/getAddressLocation';
+
 import {
 	Container,
 	ContentTexts,
@@ -9,11 +21,58 @@ import {
 	TitleModal,
 } from './styles';
 
-import MapView from 'react-native-maps';
-import Footer from '../../../../modules/global/components/Footer';
-
 const ConfirmLocation: React.FC = () => {
+	const [isLoadingLocation, setIsLoadingLocation] = useState(true);
+	const [currentAddress, setCurrentAddress] = useState<string | null>(null);
+	const [coordinates, setCoordinates] = useState<LocationObjectCoords | null>(
+		null,
+	);
 	const [modalVisible, setModalVisible] = useState(true);
+	const [locationForegroundPermission, requestLocationForegroundPermission] =
+		useForegroundPermissions();
+
+	useEffect(() => {
+		requestLocationForegroundPermission();
+	}, []);
+
+	useEffect(() => {
+		if (!locationForegroundPermission?.granted) return;
+
+		let subscription: LocationSubscription;
+
+		watchPositionAsync(
+			{
+				accuracy: LocationAccuracy.High,
+				timeInterval: 1000,
+			},
+			location => {
+				if (location.coords) {
+					setCoordinates(location.coords);
+				}
+
+				getAddressLocation(location.coords)
+					.then(address => {
+						if (address) {
+							setCurrentAddress(address);
+						}
+					})
+					.finally(() => setIsLoadingLocation(false));
+			},
+		).then(response => (subscription = response));
+
+		return () => {
+			if (subscription) subscription.remove();
+		};
+	}, [locationForegroundPermission]);
+
+	if (!locationForegroundPermission?.granted) {
+		<Container />;
+	}
+
+	if (isLoadingLocation) {
+		//retornar loading
+		return <Container />;
+	}
 
 	return (
 		<Container>
@@ -25,16 +84,29 @@ const ConfirmLocation: React.FC = () => {
 				</Description>
 			</ContentTexts>
 
-			<MapView style={{ flex: 1 }} mapType="none" />
+			<MapView
+				provider={PROVIDER_GOOGLE}
+				region={{
+					latitude: coordinates?.latitude ?? 0,
+					longitude: coordinates?.longitude ?? 0,
+					latitudeDelta: 0.005,
+					longitudeDelta: 0.005,
+				}}
+				style={{ flex: 1 }}
+				mapType="terrain">
+				<Marker
+					coordinate={{
+						latitude: coordinates?.latitude ?? 0,
+						longitude: coordinates?.longitude ?? 0,
+					}}
+				/>
+			</MapView>
 
 			{modalVisible && (
 				<ModalContainer>
 					<ModalContent>
 						<TitleModal>Minha localização</TitleModal>
-						<Description>
-							Descrição que define onde é a minha localização: Rua Eduardo Souza
-							Luiz, 179
-						</Description>
+						<Description>{currentAddress}</Description>
 
 						<Footer
 							textButton="Confirmar localização"
