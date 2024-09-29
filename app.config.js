00f25e04bf6bc8bf9a047d326627de8ca0be71a7
MateusZurdoSierra <mateus.zurdo@superlogica.com>
2024-09-29 15:39:30 -0300

    build app in expo

--- app.config.js
+++ app.config.js
@@ -6,6 +6,7 @@ module.exports = {
 		orientation: 'portrait',
 		icon: './assets/icon.png',
 		userInterfaceStyle: 'light',
+
 		splash: {
 			image: './assets/splash.png',
 			resizeMode: 'contain',
@@ -35,6 +36,15 @@ module.exports = {
 		},
 		extra: {
 			googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+			eas: {
+				projectId: '5ae3fc20-35e3-4ec7-9257-d1d5ef080881',
+			},
+		},
+		updates: {
+			url: 'https://u.expo.dev/5ae3fc20-35e3-4ec7-9257-d1d5ef080881',
+		},
+		runtimeVersion: {
+			policy: 'appVersion',
 		},
 		plugins: [
 			[
